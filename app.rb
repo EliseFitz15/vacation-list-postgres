a655bb6a89be6fdf0bd3ba46a5cca7a0f129f2b8
justinhuynh <jhuynh@gmail.com>
2015-11-24 17:00:55 -0500

    completed

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "pg"
+require "pry"
 
 configure :development do
   set :db_config, { dbname: "vacation_list_development" }
@@ -18,20 +19,57 @@ def db_connection
   end
 end
 
-FILENAME = "vacation_list.txt"
+def vacation_all
+  db_connection do |conn|
+    sql_query = "SELECT * FROM vacations"
+    conn.exec(sql_query)
+  end
+end
+
+def vacation_save(params)
+  unless params["destination"].empty?
+    db_connection do |conn|
+      sql_query = "INSERT INTO vacations (destination) VALUES ($1)"
+      data = [params["destination"]]
+      conn.exec_params(sql_query, data)
+    end
+  end
+end
+
+def vacation_find(id)
+  db_connection do |conn|
+    sql_query = "SELECT * FROM vacations WHERE id = $1"
+    data = [id]
+    conn.exec_params(sql_query, data).first
+  end
+end
+
+def vacation_comments(id)
+  db_connection do |conn|
+    sql_query = "SELECT vacations.*, comments.* FROM vacations
+        JOIN comments ON vacations.id = comments.vacation_id
+        WHERE vacations.id = $1"
+    data = [id]
+    conn.exec_params(sql_query, data)
+  end
+end
 
 get "/" do
   redirect "/vacations"
 end
 
 get "/vacations" do
-  @vacations = File.readlines(FILENAME)
+  @vacations = vacation_all
   erb :vacations
 end
 
 post "/vacations" do
-  File.open(FILENAME, "a") do |f|
-    f.puts params[:destination]
-  end
+  vacation_save(params)
   redirect "/vacations"
+end
+
+get "/vacations/:id" do
+  @vacation = vacation_find(params[:id])
+  @comments = vacation_comments(params[:id]).to_a
+  erb :show
 end
---
+++ data.sql
@@ -0,0 +1,4 @@
+INSERT INTO vacations (destination) VALUES ('Fargo');
+INSERT INTO comments (body, vacation_id) VALUES ('it is cold', 1);
+INSERT INTO comments (body, vacation_id) VALUES ('lots of space', 1);
+INSERT INTO comments (body, vacation_id) VALUES ('land', 1);
